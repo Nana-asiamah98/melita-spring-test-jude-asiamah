bd0baf11a2995b899fff04c65336744b81010d62
judeas <judeas@stlghana.com>
2023-01-12 18:38:09 +0000

    Authenticating the API Gateway

--- api-gateway/src/main/java/com/ml/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/ml/apigateway/ApiGatewayApplication.java
@@ -1,8 +1,12 @@
 package com.ml.apigateway;
 
+import io.netty.resolver.DefaultAddressResolverGroup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import reactor.netty.http.client.HttpClient;
+
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,4 +16,9 @@ public class ApiGatewayApplication {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
+
+    @Bean
+    public HttpClient httpClient(){
+        return HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE);
+    }
 }
--- order-microservice/src/test/java/com/ml/ordermicroservice/config/SecurityConfig.java
+++ order-microservice/src/test/java/com/ml/ordermicroservice/config/SecurityConfig.java
@@ -1,17 +1,11 @@
 package com.ml.ordermicroservice.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-
-@Configuration
-@EnableWebSecurity
+//@Configuration
+//@EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity){
+//    @Bean
+/*    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity){
         return  serverHttpSecurity.csrf()
                 .disable()
                 .authorizeExchange(exhcange -> exhcange
@@ -20,5 +14,5 @@ public class SecurityConfig {
                 .anyExchange()
                 .authenticated())
                 .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt).build();
-    }
+    }*/
 }
